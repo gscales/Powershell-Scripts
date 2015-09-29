826529ea6550c1d8cb93dcd51b01779d97217780
Datarumble.Glenscales <gscales@datarumble.com>
2015-09-29 16:57:42 +1000

    Unread

---
+++ unReadModule.ps1
@@ -0,0 +1,206 @@
+﻿function Connect-Exchange{ 
+    param( 
+    	[Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+		[Parameter(Position=1, Mandatory=$true)] [System.Management.Automation.PSCredential]$Credentials,
+		[Parameter(Position=2, Mandatory=$false)] [string]$url
+    )  
+ 	Begin
+		 {
+		Load-EWSManagedAPI
+		
+		## Set Exchange Version  
+		$ExchangeVersion = [Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2013_SP1
+		  
+		## Create Exchange Service Object  
+		$service = New-Object Microsoft.Exchange.WebServices.Data.ExchangeService($ExchangeVersion)  
+		  
+		## Set Credentials to use two options are availible Option1 to use explict credentials or Option 2 use the Default (logged On) credentials  
+		  
+		#Credentials Option 1 using UPN for the windows Account  
+		#$psCred = Get-Credential  
+		$creds = New-Object System.Net.NetworkCredential($Credentials.UserName.ToString(),$Credentials.GetNetworkCredential().password.ToString())  
+		$service.Credentials = $creds      
+		#Credentials Option 2  
+		#service.UseDefaultCredentials = $true  
+		 #$service.TraceEnabled = $true
+		## Choose to ignore any SSL Warning issues caused by Self Signed Certificates  
+		  
+		Handle-SSL	
+		  
+		## Set the URL of the CAS (Client Access Server) to use two options are availbe to use Autodiscover to find the CAS URL or Hardcode the CAS to use  
+		  
+		#CAS URL Option 1 Autodiscover  
+		if($url){
+			$uri=[system.URI] $url
+			$service.Url = $uri    
+		}
+		else{
+			$service.AutodiscoverUrl($MailboxName,{$true})  
+		}
+		Write-host ("Using CAS Server : " + $Service.url)   
+		   
+		#CAS URL Option 2 Hardcoded  
+		  
+		#$uri=[system.URI] "https://casservername/ews/exchange.asmx"  
+		#$service.Url = $uri    
+		  
+		## Optional section for Exchange Impersonation  
+		  
+		#$service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress, $MailboxName) 
+		if(!$service.URL){
+			throw "Error connecting to EWS"
+		}
+		else
+		{		
+			return $service
+		}
+	}
+}
+
+function Load-EWSManagedAPI{
+    param( 
+    )  
+ 	Begin
+	{
+		## Load Managed API dll  
+		###CHECK FOR EWS MANAGED API, IF PRESENT IMPORT THE HIGHEST VERSION EWS DLL, ELSE EXIT
+		$EWSDLL = (($(Get-ItemProperty -ErrorAction SilentlyContinue -Path Registry::$(Get-ChildItem -ErrorAction SilentlyContinue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Exchange\Web Services'|Sort-Object Name -Descending| Select-Object -First 1 -ExpandProperty Name)).'Install Directory') + "Microsoft.Exchange.WebServices.dll")
+		if (Test-Path $EWSDLL)
+		    {
+		    Import-Module $EWSDLL
+		    }
+		else
+		    {
+		    "$(get-date -format yyyyMMddHHmmss):"
+		    "This script requires the EWS Managed API 1.2 or later."
+		    "Please download and install the current version of the EWS Managed API from"
+		    "http://go.microsoft.com/fwlink/?LinkId=255472"
+		    ""
+		    "Exiting Script."
+		    exit
+		    } 
+  	}
+}
+
+function Handle-SSL{
+    param( 
+    )  
+ 	Begin
+	{
+		## Code From http://poshcode.org/624
+		## Create a compilation environment
+		$Provider=New-Object Microsoft.CSharp.CSharpCodeProvider
+		$Compiler=$Provider.CreateCompiler()
+		$Params=New-Object System.CodeDom.Compiler.CompilerParameters
+		$Params.GenerateExecutable=$False
+		$Params.GenerateInMemory=$True
+		$Params.IncludeDebugInformation=$False
+		$Params.ReferencedAssemblies.Add("System.DLL") | Out-Null
+
+$TASource=@'
+  namespace Local.ToolkitExtensions.Net.CertificatePolicy{
+    public class TrustAll : System.Net.ICertificatePolicy {
+      public TrustAll() { 
+      }
+      public bool CheckValidationResult(System.Net.ServicePoint sp,
+        System.Security.Cryptography.X509Certificates.X509Certificate cert, 
+        System.Net.WebRequest req, int problem) {
+        return true;
+      }
+    }
+  }
+'@ 
+		$TAResults=$Provider.CompileAssemblyFromSource($Params,$TASource)
+		$TAAssembly=$TAResults.CompiledAssembly
+
+		## We now create an instance of the TrustAll and attach it to the ServicePointManager
+		$TrustAll=$TAAssembly.CreateInstance("Local.ToolkitExtensions.Net.CertificatePolicy.TrustAll")
+		[System.Net.ServicePointManager]::CertificatePolicy=$TrustAll
+
+		## end code from http://poshcode.org/624
+
+	}
+}
+
+function CovertBitValue($String){  
+    $numItempattern = '(?=\().*(?=bytes)'  
+    $matchedItemsNumber = [regex]::matches($String, $numItempattern)   
+    $Mb = [INT64]$matchedItemsNumber[0].Value.Replace("(","").Replace(",","")  
+    return [math]::round($Mb/1048576,0)  
+}  
+
+function Get-UnReadMessageCount{
+    param( 
+    	[Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+		[Parameter(Position=1, Mandatory=$true)] [System.Management.Automation.PSCredential]$Credentials,
+		[Parameter(Position=2, Mandatory=$false)] [switch]$useImpersonation,
+		[Parameter(Position=3, Mandatory=$false)] [string]$url,
+		[Parameter(Position=4, Mandatory=$true)] [Int32]$Months
+    )  
+ 	Begin
+	{
+		$rptObj = "" | select  MailboxName,Mailboxsize,LastLogon,LastLogonAccount,LastXMonthsTotal,LastXMonthsUnread,LastMailRecieved,LastXMonthsSent,LastMailSent  
+		$rptObj.MailboxName = $MailboxName  
+		if($url){
+			$service = Connect-Exchange -MailboxName $MailboxName -Credentials $Credentials -url $url 
+		}
+		else{
+			$service = Connect-Exchange -MailboxName $MailboxName -Credentials $Credentials
+		}
+		if($useImpersonation.IsPresent){
+			$service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress, $MailboxName) 
+		}
+		$Range = [system.DateTime]::Now.AddMonths(-$Months).ToString("yyyy-MM-dd") + ".." + [system.DateTime]::Now.ToString("yyyy-MM-dd")   
+		$AQSString1 = "System.Message.DateReceived:" + $Range   
+  		$folderid= new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::Inbox,$MailboxName)     
+		$Inbox = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)  
+  		$folderid= new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::SentItems,$MailboxName)     
+		$SentItems = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)    
+		$ivItemView = New-Object Microsoft.Exchange.WebServices.Data.ItemView(1000)  
+		$psPropset= new-object Microsoft.Exchange.WebServices.Data.PropertySet([Microsoft.Exchange.WebServices.Data.BasePropertySet]::IdOnly)  
+		$psPropset.Add([Microsoft.Exchange.WebServices.Data.ItemSchema]::DateTimeReceived)
+		$psPropset.Add([Microsoft.Exchange.WebServices.Data.EmailMessageSchema]::IsRead)
+		$ivItemView.PropertySet = $psPropset
+  		$MailboxStats = Get-MailboxStatistics $MailboxName  
+		$ts = CovertBitValue($MailboxStats.TotalItemSize.ToString())  
+		write-host ("Total Size : " + $MailboxStats.TotalItemSize) 
+	    $rptObj.MailboxSize = $ts  
+		write-host ("Last Logon Time : " + $MailboxStats.LastLogonTime) 
+		$rptObj.LastLogon = $MailboxStats.LastLogonTime  
+		write-host ("Last Logon Account : " + $MailboxStats.LastLoggedOnUserAccount ) 
+		$rptObj.LastLogonAccount = $MailboxStats.LastLoggedOnUserAccount  
+		$fiItems = $Inbox.findItems($AQSString1,$ivItemView)  
+		$rptObj.LastXMonthsTotal = $fiItems.TotalCount  
+		write-host ("Last " + $Months + " Months : " + $fiItems.TotalCount)
+		if($fiItems.TotalCount -gt 0){  
+		    write-host ("Last Mail Recieved : " + $fiItems.Items[0].DateTimeReceived ) 
+		    $rptObj.LastMailRecieved = $fiItems.Items[0].DateTimeReceived  
+		}
+		$unreadCount = 0
+		if($fiItems.MoreAvailable){
+	   		do{    
+			    $fiItems = $service.FindItems($Inbox.Id,$ivItemView)		    
+			    foreach($Item in $fiItems.Items){
+					$unReadVal = $null
+					if($Item.TryGetProperty([Microsoft.Exchange.WebServices.Data.EmailMessageSchema]::IsRead,[ref]$unReadVal)){
+						if(!$unReadVal){
+							$unreadCount++
+						}
+					}      
+			    }    
+			    $ivItemView.Offset += $fiItems.Items.Count    
+			}while($fiItems.MoreAvailable -eq $true) 
+		}
+		write-host ("Last " + $Months + " Months Unread : " + $unreadCount ) 
+		$rptObj.LastXMonthsUnread = $unreadCount  
+		$ivItemView = New-Object Microsoft.Exchange.WebServices.Data.ItemView(1)  
+		$fiResults = $SentItems.findItems($AQSString1,$ivItemView)  
+		write-host ("Last " + $Months + " Months Sent : " + $fiResults.TotalCount  )
+		$rptObj.LastXMonthsSent = $fiResults.TotalCount  
+		if($fiResults.TotalCount -gt 0){  
+		    write-host ("Last Mail Sent Date : " + $fiResults.Items[0].DateTimeSent  )
+		    $rptObj.LastMailSent = $fiResults.Items[0].DateTimeSent  
+		}  
+		Write-Output $rptObj  
+	}
+}

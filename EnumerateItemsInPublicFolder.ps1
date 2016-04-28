ced64b6c1178571e1b5df7c7976f887f9ab1e2e9
Datarumble.Glenscales <gscales@datarumble.com>
2016-04-28 19:32:34 +1000

    updates for public folder

--- EnumerateItemsInPublicFolder.ps1
+++ EnumerateItemsInPublicFolder.ps1
@@ -34,8 +34,11 @@ function Get-PublicFolderItems
 		{
 			$service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress, $MailboxName) 
 		}
-		$fldId = FolderIdFromPath -FolderPath $PublicFolderPath  -SmtpAddress $MailboxName
-		$SubFolderId =  new-object Microsoft.Exchange.WebServices.Data.FolderId($fldId)
+        Get-PublicFolderRoutingHeader -service $service -Credentials $Credentials -MailboxName $MailboxName -Header "X-AnchorMailbox"
+		$fldId = PublicFolderIdFromPath -FolderPath $PublicFolderPath  -SmtpAddress $MailboxName -service $service		
+        $SubFolderId =  new-object Microsoft.Exchange.WebServices.Data.FolderId($fldId)
+         
+        
 		#Define ItemView to retrive just 1000 Items    
 		$ivItemView =  New-Object Microsoft.Exchange.WebServices.Data.ItemView(1000) 
 		$ItemPropset= new-object Microsoft.Exchange.WebServices.Data.PropertySet([Microsoft.Exchange.WebServices.Data.BasePropertySet]::FirstClassProperties)
@@ -179,23 +182,36 @@ $TASource=@'
 
 	}
 }
-function FolderIdFromPath{
+function PublicFolderIdFromPath{
 	param (
-	        $FolderPath = "$( throw 'Folder Path is a mandatory Parameter' )",
-			$SmtpAddress = "$( throw 'Folder Path is a mandatory Parameter' )"
+            [Parameter(Position=0, Mandatory=$true)] [Microsoft.Exchange.WebServices.Data.ExchangeService]$service,
+            [Parameter(Position=1, Mandatory=$true)] [String]$FolderPath,
+            [Parameter(Position=2, Mandatory=$true)] [String]$SmtpAddress
 		  )
 	process{
 		## Find and Bind to Folder based on Path  
 		#Define the path to search should be seperated with \  
 		#Bind to the MSGFolder Root  
 		$folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::PublicFoldersRoot)   
-		$tfTargetFolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)  
+        $psPropset= new-object Microsoft.Exchange.WebServices.Data.PropertySet([Microsoft.Exchange.WebServices.Data.BasePropertySet]::FirstClassProperties)
+        $PR_REPLICA_LIST = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x6698,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Binary); 
+        $psPropset.Add($PR_REPLICA_LIST)
+		$tfTargetFolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid,$psPropset)  
+        $PR_REPLICA_LIST_Value = $null
+        if($tfTargetFolder.TryGetProperty($PR_REPLICA_LIST,[ref]$PR_REPLICA_LIST_Value)){
+                 $GuidAsString = [System.Text.Encoding]::ASCII.GetString($PR_REPLICA_LIST_Value, 0, 36);
+                 $HeaderAddress = new-object System.Net.Mail.MailAddress($service.HttpHeaders["X-AnchorMailbox"])
+                 $pfHeader = $GuidAsString + "@" + $HeaderAddress.Host
+                 write-host ("Root Public Folder Routing Information Header : " + $pfHeader )  
+                 $service.HttpHeaders.Add("X-PublicFolderMailbox", $pfHeader)    
+        }
 		#Split the Search path into an array  
 		$fldArray = $FolderPath.Split("\") 
 		 #Loop through the Split Array and do a Search for each level of folder 
 		for ($lint = 1; $lint -lt $fldArray.Length; $lint++) { 
 	        #Perform search based on the displayname of each folder level 
 	        $fvFolderView = new-object Microsoft.Exchange.WebServices.Data.FolderView(1) 
+            $fvFolderView.PropertySet = $psPropset
 	        $SfSearchFilter = new-object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName,$fldArray[$lint]) 
 	        $findFolderResults = $service.FindFolders($tfTargetFolder.Id,$SfSearchFilter,$fvFolderView) 
 	        if ($findFolderResults.TotalCount -gt 0){ 
@@ -210,10 +226,104 @@ function FolderIdFromPath{
 	        }     
 	    }  
 		if($tfTargetFolder -ne $null){
+            $PR_REPLICA_LIST_Value = $null
+            if($tfTargetFolder.TryGetProperty($PR_REPLICA_LIST,[ref]$PR_REPLICA_LIST_Value)){
+                    $GuidAsString = [System.Text.Encoding]::ASCII.GetString($PR_REPLICA_LIST_Value, 0, 36);
+                    $HeaderAddress = new-object System.Net.Mail.MailAddress($service.HttpHeaders["X-AnchorMailbox"])
+                    $pfHeader = $GuidAsString + "@" + $HeaderAddress.Host
+                    write-host ("Target Public Folder Routing Information Header : " + $pfHeader )  
+                    Get-PublicFolderContentRoutingHeader -service $service -Credentials $Credentials -MailboxName $SmtpAddress -pfAddress $pfHeader
+            }            
 			return $tfTargetFolder.Id.UniqueId.ToString()
 		}
 		else{
 			throw "Folder not found"
 		}
 	}
+}
+
+function Get-PublicFolderRoutingHeader
+{
+    param (
+	        [Parameter(Position=0, Mandatory=$true)] [Microsoft.Exchange.WebServices.Data.ExchangeService]$service,
+            [Parameter(Position=1, Mandatory=$true)] [PSCredential]$Credentials,
+		    [Parameter(Position=2, Mandatory=$true)] [string]$MailboxName,
+            [Parameter(Position=3, Mandatory=$true)] [string]$Header
+          )
+	process
+    {
+        $ExchangeVersion = [Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2013_SP1
+        $AutoDiscoverService =  New-Object  Microsoft.Exchange.WebServices.Autodiscover.AutodiscoverService($ExchangeVersion);
+        $creds = New-Object System.Net.NetworkCredential($Credentials.UserName.ToString(),$Credentials.GetNetworkCredential().password.ToString()) 
+        $AutoDiscoverService.Credentials = $creds
+        $AutoDiscoverService.EnableScpLookup = $false;
+        $AutoDiscoverService.RedirectionUrlValidationCallback = {$true};
+        $AutoDiscoverService.PreAuthenticate = $true;
+        $AutoDiscoverService.KeepAlive = $false;      
+        if($Header -eq "X-AnchorMailbox")
+        {
+            $gsp = $AutoDiscoverService.GetUserSettings($MailboxName,[Microsoft.Exchange.WebServices.Autodiscover.UserSettingName]::PublicFolderInformation);
+            $PublicFolderInformation = $null
+            if ($gsp.Settings.TryGetValue([Microsoft.Exchange.WebServices.Autodiscover.UserSettingName]::PublicFolderInformation, [ref] $PublicFolderInformation))
+            {
+                write-host ("Public Folder Routing Information Header : " + $PublicFolderInformation)  
+                $service.HttpHeaders.Add($Header, $PublicFolderInformation)           
+                        
+            } 
+            
+        }
+
+       
+    }
+    
+}
+function Get-PublicFolderContentRoutingHeader
+{
+    param (
+	        [Parameter(Position=0, Mandatory=$true)] [Microsoft.Exchange.WebServices.Data.ExchangeService]$service,
+            [Parameter(Position=1, Mandatory=$true)] [PSCredential]$Credentials,
+		    [Parameter(Position=2, Mandatory=$true)] [string]$MailboxName,
+            [Parameter(Position=3, Mandatory=$true)] [string]$pfAddress
+     )
+	process
+    {
+        $ExchangeVersion = [Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2013_SP1
+        $AutoDiscoverService =  New-Object  Microsoft.Exchange.WebServices.Autodiscover.AutodiscoverService($ExchangeVersion);
+        $creds = New-Object System.Net.NetworkCredential($Credentials.UserName.ToString(),$Credentials.GetNetworkCredential().password.ToString()) 
+        $AutoDiscoverService.Credentials = $creds
+        $AutoDiscoverService.EnableScpLookup = $false;
+        $AutoDiscoverService.RedirectionUrlValidationCallback = {$true};
+        $AutoDiscoverService.PreAuthenticate = $true;
+        $AutoDiscoverService.KeepAlive = $false;      
+        $gsp = $AutoDiscoverService.GetUserSettings($MailboxName,[Microsoft.Exchange.WebServices.Autodiscover.UserSettingName]::AutoDiscoverSMTPAddress);
+        #Write-Host $AutoDiscoverService.url
+        $auDisXML = "<Autodiscover xmlns=`"http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006`"><Request>`r`n" +
+        "<EMailAddress>" + $pfAddress + "</EMailAddress>`r`n" +
+        "<AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema>`r`n" +
+        "</Request>`r`n" +
+        "</Autodiscover>`r`n";
+        $AutoDiscoverRequest = [System.Net.HttpWebRequest]::Create($AutoDiscoverService.url.ToString().replace(".svc",".xml"));
+        $bytes = [System.Text.Encoding]::UTF8.GetBytes($auDisXML);
+        $AutoDiscoverRequest.ContentLength = $bytes.Length;
+        $AutoDiscoverRequest.ContentType = "text/xml";
+        $AutoDiscoverRequest.UserAgent = "Microsoft Office/16.0 (Windows NT 6.3; Microsoft Outlook 16.0.6001; Pro)";            
+        $AutoDiscoverRequest.Headers.Add("Translate", "F");
+        $AutoDiscoverRequest.Method = "POST";
+        $AutoDiscoverRequest.Credentials = $creds;
+        $RequestStream = $AutoDiscoverRequest.GetRequestStream();
+        $RequestStream.Write($bytes, 0, $bytes.Length);
+        $RequestStream.Close();
+        $AutoDiscoverRequest.AllowAutoRedirect = $truee;
+        $Response = $AutoDiscoverRequest.GetResponse().GetResponseStream()
+        $sr = New-Object System.IO.StreamReader($Response)
+        [XML]$xmlReposne = $sr.ReadToEnd()
+        if($xmlReposne.Autodiscover.Response.User.AutoDiscoverSMTPAddress -ne $null)
+        {
+            write-host ("Public Folder Content Routing Information Header : " + $xmlReposne.Autodiscover.Response.User.AutoDiscoverSMTPAddress)  
+            $service.HttpHeaders["X-AnchorMailbox"] = $xmlReposne.Autodiscover.Response.User.AutoDiscoverSMTPAddress    
+            $service.HttpHeaders["X-PublicFolderMailbox"] = $xmlReposne.Autodiscover.Response.User.AutoDiscoverSMTPAddress              
+        }
+
+    }
+    
 }

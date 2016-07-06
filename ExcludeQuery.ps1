59d805d000e2d7d1253b0977a60d3602be91c829
Datarumble.Glenscales <gscales@datarumble.com>
2016-07-06 20:46:11 +1000

    Exclude Query

---
+++ ExcludeQuery.ps1
@@ -0,0 +1,214 @@
+function Connect-Exchange{ 
+    param( 
+    	[Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+		[Parameter(Position=1, Mandatory=$true)] [System.Management.Automation.PSCredential]$Credentials,
+		[Parameter(Position=2, Mandatory=$false)] [string]$url
+    )  
+ 	Begin
+		 {
+		Load-EWSManagedAPI
+		
+		## Set Exchange Version  
+		$ExchangeVersion = [Microsoft.Exchange.WebServices.Data.ExchangeVersion]::Exchange2013
+		  
+		## Create Exchange Service Object  
+		$service = New-Object Microsoft.Exchange.WebServices.Data.ExchangeService($ExchangeVersion)  
+		  
+		## Set Credentials to use two options are availible Option1 to use explict credentials or Option 2 use the Default (logged On) credentials  
+		  
+		#Credentials Option 1 using UPN for the windows Account  
+		#$psCred = Get-Credential  
+		$creds = New-Object System.Net.NetworkCredential($Credentials.UserName.ToString(),$Credentials.GetNetworkCredential().password.ToString())  
+		$service.Credentials = $creds      
+		#Credentials Option 2  
+		#service.UseDefaultCredentials = $true  
+		 #$service.TraceEnabled = $true
+		## Choose to ignore any SSL Warning issues caused by Self Signed Certificates  
+		  
+		Handle-SSL	
+		  
+		## Set the URL of the CAS (Client Access Server) to use two options are availbe to use Autodiscover to find the CAS URL or Hardcode the CAS to use  
+		  
+		#CAS URL Option 1 Autodiscover  
+		if($url){
+			$uri=[system.URI] $url
+			$service.Url = $uri    
+		}
+		else{
+			$service.AutodiscoverUrl($MailboxName,{$true})  
+		}
+		Write-host ("Using CAS Server : " + $Service.url)   
+		   
+		#CAS URL Option 2 Hardcoded  
+		  
+		#$uri=[system.URI] "https://casservername/ews/exchange.asmx"  
+		#$service.Url = $uri    
+		  
+		## Optional section for Exchange Impersonation  
+		  
+		#$service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress, $MailboxName) 
+		if(!$service.URL){
+			throw "Error connecting to EWS"
+		}
+		else
+		{		
+			return $service
+		}
+	}
+}
+
+function Load-EWSManagedAPI{
+    param( 
+    )  
+ 	Begin
+	{
+		## Load Managed API dll  
+		###CHECK FOR EWS MANAGED API, IF PRESENT IMPORT THE HIGHEST VERSION EWS DLL, ELSE EXIT
+		$EWSDLL = (($(Get-ItemProperty -ErrorAction SilentlyContinue -Path Registry::$(Get-ChildItem -ErrorAction SilentlyContinue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Exchange\Web Services'|Sort-Object Name -Descending| Select-Object -First 1 -ExpandProperty Name)).'Install Directory') + "Microsoft.Exchange.WebServices.dll")
+		if (Test-Path $EWSDLL)
+		    {
+		    Import-Module $EWSDLL
+		    }
+		else
+		    {
+		    "$(get-date -format yyyyMMddHHmmss):"
+		    "This script requires the EWS Managed API 1.2 or later."
+		    "Please download and install the current version of the EWS Managed API from"
+		    "http://go.microsoft.com/fwlink/?LinkId=255472"
+		    ""
+		    "Exiting Script."
+		    exit
+		    } 
+  	}
+}
+
+function Handle-SSL{
+    param( 
+    )  
+ 	Begin
+	{
+		## Code From http://poshcode.org/624
+		## Create a compilation environment
+		$Provider=New-Object Microsoft.CSharp.CSharpCodeProvider
+		$Compiler=$Provider.CreateCompiler()
+		$Params=New-Object System.CodeDom.Compiler.CompilerParameters
+		$Params.GenerateExecutable=$False
+		$Params.GenerateInMemory=$True
+		$Params.IncludeDebugInformation=$False
+		$Params.ReferencedAssemblies.Add("System.DLL") | Out-Null
+
+$TASource=@'
+  namespace Local.ToolkitExtensions.Net.CertificatePolicy{
+    public class TrustAll : System.Net.ICertificatePolicy {
+      public TrustAll() { 
+      }
+      public bool CheckValidationResult(System.Net.ServicePoint sp,
+        System.Security.Cryptography.X509Certificates.X509Certificate cert, 
+        System.Net.WebRequest req, int problem) {
+        return true;
+      }
+    }
+  }
+'@ 
+		$TAResults=$Provider.CompileAssemblyFromSource($Params,$TASource)
+		$TAAssembly=$TAResults.CompiledAssembly
+
+		## We now create an instance of the TrustAll and attach it to the ServicePointManager
+		$TrustAll=$TAAssembly.CreateInstance("Local.ToolkitExtensions.Net.CertificatePolicy.TrustAll")
+		[System.Net.ServicePointManager]::CertificatePolicy=$TrustAll
+
+		## end code from http://poshcode.org/624
+
+	}
+}
+
+function FolderIdFromPath{
+	param (
+	        $FolderPath = "$( throw 'Folder Path is a mandatory Parameter' )",
+			$SmtpAddress = "$( throw 'Folder Path is a mandatory Parameter' )"
+		  )
+	process{
+		## Find and Bind to Folder based on Path  
+		#Define the path to search should be seperated with \  
+		#Bind to the MSGFolder Root  
+		$folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot,$SmtpAddress)   
+		$tfTargetFolder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)  
+		#Split the Search path into an array  
+		$fldArray = $FolderPath.Split("\") 
+		 #Loop through the Split Array and do a Search for each level of folder 
+		for ($lint = 1; $lint -lt $fldArray.Length; $lint++) { 
+	        #Perform search based on the displayname of each folder level 
+	        $fvFolderView = new-object Microsoft.Exchange.WebServices.Data.FolderView(1) 
+	        $SfSearchFilter = new-object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName,$fldArray[$lint]) 
+	        $findFolderResults = $service.FindFolders($tfTargetFolder.Id,$SfSearchFilter,$fvFolderView) 
+	        if ($findFolderResults.TotalCount -gt 0){ 
+	            foreach($folder in $findFolderResults.Folders){ 
+	                $tfTargetFolder = $folder                
+	            } 
+	        } 
+	        else{ 
+	            "Error Folder Not Found"  
+	            $tfTargetFolder = $null  
+	            break  
+	        }     
+	    }  
+		if($tfTargetFolder -ne $null){
+			return $tfTargetFolder.Id
+		}
+		else{
+			throw "Folder not found"
+		}
+	}
+}
+function Query-MessageFlag  {
+	    param( 
+    	[Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+		[Parameter(Position=1, Mandatory=$true)] [System.Management.Automation.PSCredential]$Credentials,
+		[Parameter(Position=2, Mandatory=$false)] [switch]$useImpersonation,
+		[Parameter(Position=3, Mandatory=$false)] [string]$url,
+        [Parameter(Position=4, Mandatory=$true)] [Int32]$Flag,
+		[Parameter(Position=5, Mandatory=$true)] [string]$FolderPath,
+		[Parameter(Position=6, Mandatory=$false)] [switch]$Negate,
+		[Parameter(Position=7, Mandatory=$false)] [switch]$SummaryOnly
+    )  
+ 	Begin
+	{
+		if($url){
+			$service = Connect-Exchange -MailboxName $MailboxName -Credentials $Credentials -url $url 
+		}
+		else{
+			$service = Connect-Exchange -MailboxName $MailboxName -Credentials $Credentials
+		}
+		if($useImpersonation.IsPresent){
+			$service.ImpersonatedUserId = new-object Microsoft.Exchange.WebServices.Data.ImpersonatedUserId([Microsoft.Exchange.WebServices.Data.ConnectingIdType]::SmtpAddress, $MailboxName) 
+		}
+ 		$folderId = FolderIdFromPath -FolderPath $FolderPath -SmtpAddress $MailboxName
+		if($folderId -ne $null){
+			$Folder = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderId) 
+			Write-Host ("Folder Name : " + $Folder.DisplayName)
+			$PR_MESSAGE_FLAGS = new-object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x0E07,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::Integer); 
+			$sfItemSearchFilter = new-object Microsoft.Exchange.WebServices.Data.SearchFilter+ExcludesBitmask($PR_MESSAGE_FLAGS, $Flag) 
+            if($Negate.IsPresent){
+				$sfItemSearchFilter = new-object Microsoft.Exchange.WebServices.Data.SearchFilter+Not($sfItemSearchFilter)
+			}   
+			if(!$SummaryOnly.IsPresent){
+				$ivItemView =  New-Object Microsoft.Exchange.WebServices.Data.ItemView(1000)
+				$fiItems = $null    
+				do{    
+					$fiItems = $service.FindItems($Folder.Id,$sfItemSearchFilter,$ivItemView)    
+					#[Void]$service.LoadPropertiesForItems($fiItems,$psPropset)  
+					foreach($Item in $fiItems.Items){      
+					    Write-Host($Item.DateTimeReceived + " : " + $Item.Subject)       
+					}    
+					$ivItemView.Offset += $fiItems.Items.Count    
+				}while($fiItems.MoreAvailable -eq $true) 
+			}
+			else
+			{
+				 $ivItemView =  New-Object Microsoft.Exchange.WebServices.Data.ItemView(1)
+				 $fiItems = $service.FindItems($Folder.Id,$sfItemSearchFilter,$ivItemView)  
+				 Write-Host ("Total Items : " + $fiItems.TotalCount)
+			}
+		}
+	}
+}
